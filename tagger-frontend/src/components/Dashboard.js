40a30e8a8b3af7d6e30fdc494295c83a9c799e1e
serg-e <smosesov@gmail.com>
2020-06-24 11:03:04 +0100

    Added Folders Component in Dashboard

--- tagger-frontend/src/components/Dashboard.js
+++ tagger-frontend/src/components/Dashboard.js
@@ -2,45 +2,19 @@ import React, { useState, useEffect, Fragment } from 'react';
 
 const Dashboard = ({ user }) => {
 
+    const [folders, setFolders] = useState([
+        {'id': '1234', 'name': 'NeoNotesPDF'},
+        {'id': '4354', 'name': 'Another Folder'},
+    ])
 
+    const setIsWatched = (folderId) => {
+        return
+    }
     return (
         <Fragment>
         <div class="container">
-            <div class="my-3 p-3 bg-white rounded shadow-sm">
-
-                <h6 class="border-bottom border-gray pb-2 mb-0">Google Drive Folders</h6>
-
-
-                <div class="media text-muted pt-3">
-                    <h2 className="d-flex mr-3">
-                <i class="far fa-folder  "></i></h2>
-                    {/* <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff" /><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg> */}
-                    <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                        <strong class="d-block text-gray-dark">Folder Name</strong>
-        <em>last synced: 12:00 today</em>
-      </p>
-                </div>
-
-
-
-                <div class="media text-muted pt-3">
-                    <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c" /><text x="50%" y="50%" fill="#e83e8c" dy=".3em">32x32</text></svg>
-                    <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                        <strong class="d-block text-gray-dark">@username</strong>
-        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-      </p>
-                </div>
-                <div class="media text-muted pt-3">
-                    <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title> <i class="far fa-folder"></i></title><rect width="100%" height="100%" fill="#6f42c1" /><text x="50%" y="50%" fill="#6f42c1" dy=".3em">32x32</text></svg>
-                    <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                        <strong class="d-block text-gray-dark">@username</strong>
-        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-      </p>
-                </div>
-                <small class="d-block text-right mt-3">
-                    <a href="#">All updates</a>
-                </small>
-            </div>
+            <Folders title={"G-Drive Folders"} folders={folders} />
+                            <Folders title={"Evernote"} folders={folders} />
             {/* <div >
                 <a href='/login'> Login </a>
                 <a href='/logout'> Logout </a>
@@ -48,10 +22,47 @@ const Dashboard = ({ user }) => {
                 <a href="/get_evernote_tags"> All tags</a>
             </div> */}
         </div >
-  
+
 
         </Fragment>
     )
 }
 
+
+
+const Folders = ({title, folders, setIsWatched}) => {
+    return (
+
+            <div class="my-3 p-3 bg-success nreen rounded shadow-sm">
+                <h6 class="border-bottom border-gray pb-2 mb-0">{title}</h6>
+                    {folders.map((folder)=> (<Folder setIsWatched={setIsWatched} folder={folder}/>))}
+                <small class="d-block text-right mt-3">
+                    <a href="#">All updates</a>
+                </small>
+            </div>
+    )
+}
+
+
+// isWatched, destinations
+
+
+const Folder = ({folder}) =>{
+
+    const {name, timeSynced, isWatched, setIsWatched} = folder
+    return (
+                <div class="media text-white pt-3">
+                    <h2 className="d-flex mr-3">
+                <i class="far fa-folder  "></i></h2>
+                    <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                        <strong class="d-block text-gray-dark">{name}</strong>
+                            <em>last synced: {timeSynced}</em>
+
+            </p>
+
+          </div>
+)
+}
+
+// https://gitbrent.github.io/bootstrap4-toggle/
 export default Dashboard
